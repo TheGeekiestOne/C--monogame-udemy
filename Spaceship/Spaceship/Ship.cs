b25771ba92731c7272e05bf4a1c4277887b03c25
Ayran Olckers <w1654684@my.westminster.ac.uk>
2019-02-27 12:06:38 +0000

    update new class ship.cs

---
+++ Spaceship/Spaceship/Ship.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceship
+{
+    class Ship
+    {
+
+        public Vector2 postition = new Vector2(100, 100);
+
+
+    }
+}
