7bddb5688336f15a2e1b946682f2ef5b7bb8a494
Ayran Olckers <w1654684@my.westminster.ac.uk>
2019-02-28 10:22:22 +0000

    structs lecture

--- practiceCode/practiceCode2/practiceCode2/Program.cs
+++ practiceCode/practiceCode2/practiceCode2/Program.cs
@@ -6,16 +6,43 @@ using System.Threading.Tasks;
 
 namespace practiceCode2
 {
+
+    struct House
+    {
+        public string address;
+        public int width;
+        public int length;
+
+        public House(string a, int w, int l)
+        {
+            address = a;
+            width = w;
+            length = l;
+        }
+
+        public int Area()
+        {
+            return width * length;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            House myHouse = new House("789 road Street", 20, 10);
+            /*
+            myHouse.address = "123 Streeto";
+            myHouse.width = 50;
+            myHouse.length = 35;
+            */
+
+            Console.WriteLine(myHouse.address);
+            Console.WriteLine(myHouse.Area());
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            
         }
     }
 }
